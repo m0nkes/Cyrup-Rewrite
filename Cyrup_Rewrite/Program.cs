85a60a41e8ee9442237aee0db3f7e4aa1549f4df
deadlocust <70506265+deaddlocust@users.noreply.github.com>
2021-12-05 16:26:07 +0800

    Initial commit

---
+++ Cyrup_Rewrite/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using static Cyrup_Rewrite.Native;
+
+namespace Cyrup_Rewrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(80, 20);
+            if (!Directory.Exists(Environment.CurrentDirectory + "\\autoexec")) Directory.CreateDirectory(Environment.CurrentDirectory + "\\autoexec");
+            if (!Directory.Exists(Environment.CurrentDirectory + "\\scripts")) Directory.CreateDirectory(Environment.CurrentDirectory + "\\scripts");
+
+            IntPtr consolehwnd = GetConsoleWindow();
+            if (consolehwnd == IntPtr.Zero)
+            {
+                Console.WriteLine("fatal error: failed to grab console window handle");
+                Thread.Sleep(3000);
+                Environment.Exit(1);
+            }
+
+            IntPtr menuhandle = GetSystemMenu(consolehwnd, false);
+            DeleteMenu(menuhandle, SC_MAXIMIZE, 0); // Disable maximize
+            DeleteMenu(menuhandle, SC_SIZE, 0); // Disable resize
+
+            Interface gui = new Interface("Cyrup");
+            gui.Start();
+        }
+    }
+}
